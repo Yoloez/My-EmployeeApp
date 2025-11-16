541cef0fc7c511ca06e90212aff6bde4f7c64f7f
hanan <hananfijananto@gmail.com>
2025-11-16 10:57:03 +0700

    tambah fitur edit delete

--- app/src/main/java/com/example/employeeapp/model/Employee.kt
+++ app/src/main/java/com/example/employeeapp/model/Employee.kt
@@ -7,12 +7,12 @@ data class Employee(
     @SerializedName(value = "id")
     val id: Int,
 
-    @SerializedName(value = "name")
+    @SerializedName(value = "employee_name")
     val name: String,
 
-    @SerializedName(value = "salary")
+    @SerializedName(value = "employee_salary")
     val salary: Int,
 
-    @SerializedName(value = "age")
+    @SerializedName(value = "employee_age")
     val age: Int
 )
